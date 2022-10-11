1dce8da796a0fe7952c37bada586761d129a4e43
Kyuwoong Lee <kwooler@gmail.com>
2022-10-10 22:35:26 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include        <iostream>
+#include        <iomanip>
+using   namespace       std;
+
+int main()
+{
+    int i, begin, end, sum=0 ;
+
+	// complete your code
+  
+    cout << "The sum value is " << sum << endl;
+}
